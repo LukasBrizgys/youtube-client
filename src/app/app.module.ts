2fbe8c5d7cf6708814153475087cb386686bbf5f
Lukas Brizgys <lukenis456@gmail.com>
2022-08-03 12:08:56 +0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { CardComponent } from './card/card.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    CardComponent,
+    RegistrationComponent,
+    LoginComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
