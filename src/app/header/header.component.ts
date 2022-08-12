b733950a77daa0d181107acbf7297a2b34108f6f
Lukas Brizgys <lukenis456@gmail.com>
2022-08-12 13:26:30 +0300

    feat: add settings toggle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 class HeaderComponent implements OnInit {
+  searchKeyword = '';
+  @Output() keywordEvent = new EventEmitter<string>();
+  @Output() showSearchPageEvent = new EventEmitter<boolean>();
+  @Output() toggleSettingsEvent = new EventEmitter<any>();
   ngOnInit(): void {
+  }
+
+  handleSearch(keyword : string) {
+    this.searchKeyword = keyword;
+    this.keywordEvent.emit(this.searchKeyword);
+    this.showSearchPageEvent.emit(true);
+  }
+  toggleSettings() {
+    this.toggleSettingsEvent.emit();
   }
 }
 export default HeaderComponent;
