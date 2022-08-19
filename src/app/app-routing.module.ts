389e0333cdcecafe3236c91236f1d79287136fb6
Lukas Brizgys <lukenis456@gmail.com>
2022-08-19 11:49:49 +0300

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import AuthGuard from './core/guards/auth/auth.guard';
+import PageNotFoundComponent from './core/pages/page-not-found/page-not-found.component';
+const routes: Routes = [
+  { path: '', title: 'dashboard', canActivate: [AuthGuard], canLoad: [AuthGuard], children: [
+    { path: 'videos', loadChildren: () => import('./youtube/youtube.module').then(m => m.default)},
+  ]},
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.default) },
+  { path: '**', component: PageNotFoundComponent, title: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
