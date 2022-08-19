46070a9df23bbe963cd28ce3ab41a90bde6ef8be
Lukas Brizgys <lukenis456@gmail.com>
2022-08-19 11:52:24 +0300

    feat: add login service

---
+++ src/app/auth/service/login.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+class LoginService {
+  isLoggedIn : boolean = false;
+  constructor(private router : Router) {
+    this.isLoggedIn = localStorage.getItem('auth-token') ? true : false;
+  }
+  isAuthenticated() : boolean {
+    return this.isLoggedIn;
+  }
+
+  handleLogin() : void {
+    localStorage.setItem('auth-token', crypto.randomUUID());
+    this.isLoggedIn = true;
+    this.router.navigate(['/']);
+  }
+  handleLogout() : void {
+    localStorage.removeItem('auth-token');
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
+}
+export default LoginService;
