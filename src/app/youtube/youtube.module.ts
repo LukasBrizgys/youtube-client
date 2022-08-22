bacd669cab4990d238311310af927ea2c0ca27e0
Lukas Brizgys <lukenis456@gmail.com>
2022-08-22 22:57:41 +0300

    fix: delete shared module

--- src/app/youtube/youtube.module.ts
+++ src/app/youtube/youtube.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import CardComponent from './components/card/card.component';
 import CardDirective from './directives/card.directive';
 import FilterPipe from './pipes/filter/filter.pipe';
-import SharedModule from '../shared/shared.module';
 import YoutubeRoutingModule from './youtube-routing.module';
 import SearchPageComponent from './pages/search-page/search-page.component';
 import PreviewComponent from './pages/preview/preview.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import PreviewComponent from './pages/preview/preview.component';
   ],
   imports: [
     CommonModule,
-    SharedModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
     YoutubeRoutingModule,
   ],
   exports: [
