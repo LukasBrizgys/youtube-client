7c52fe41a8b16d782ee4d044d41e7de1997bba62
Lukas Brizgys <lukenis456@gmail.com>
2022-08-25 13:04:57 +0300

    fix: remove auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import AuthGuard from './core/guards/auth/auth.guard';
 import PageNotFoundComponent from './core/pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     title: 'dashboard',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
     loadChildren: () => import('./youtube/youtube.module').then((m) => m.default),
   },
 
