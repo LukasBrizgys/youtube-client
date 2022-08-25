8e25ec81051e6804982ab81c0db1b3dbc3e5a70e
Lukas Brizgys <lukenis456@gmail.com>
2022-08-25 12:45:03 +0300

    feat: add interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import CoreModule from './core/core.module';
+import TokenInterceptor from './core/interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import CoreModule from './core/core.module';
     CoreModule,
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent],
 })
 class AppModule { }
